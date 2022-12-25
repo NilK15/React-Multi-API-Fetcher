ac53dcd7831c93a8909dc99a12b588235772fe82
NilK15 <skhatri15@outlook.com>
2022-12-25 13:22:45 -0500

    added some more css updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,52 +1,58 @@
 // import logo from "../svg/nasa.svg";
 import "../styles/header.css";
 import nasapic from "../img/space.jpg";
-
-const BODY_BACKGROUND_NASA_DIV = "bodybackgroundnasadiv";
-const BODY_BACKGROUND_DOG_DIV = "bodybackgrounddogdiv";
-const BODY_BACKGROUND_POKEMON_DIV = "bodybackgroundpokemondiv";
-const BODY_BACKGROUND_GOOGLE_DIV = "bodybackgroundgooglediv";
-
-const removeDivs = (div) => {
-  // let divArray = ["nasadiv", "dogdiv", "pokemondiv", "googlediv"];
-  let divArray = ["nasadiv", "dogdiv", "pokemondiv"];
-  let arrayWithoutExcludedDiv = [];
-  divArray.filter((e) => e !== div).map((e) => arrayWithoutExcludedDiv.push(e));
-
-  arrayWithoutExcludedDiv.forEach((e) => {
-    let divToRemove = document.getElementsByClassName(e);
-    if (divToRemove[0].classList.contains(`${e}show`)) {
-      divToRemove[0].classList.remove(`${e}show`);
-    }
-  });
-  document.body.classList.remove(
-    BODY_BACKGROUND_NASA_DIV,
-    BODY_BACKGROUND_DOG_DIV,
-    BODY_BACKGROUND_POKEMON_DIV,
-    BODY_BACKGROUND_GOOGLE_DIV
-  );
-  const pokestring = `bodybackground${div}`;
-  document.body.classList.add(`bodybackground${div}`);
-  let divToShow = document.getElementsByClassName(div);
-  divToShow[0].classList.add(`${div}show`);
-};
-
-const hideShow = (innerHTML, divToShow) => {
-  let buttons = document.getElementsByClassName("navbutton");
-  for (const element of buttons) {
-    if (element.innerHTML === innerHTML) {
-      element.classList.add("selected");
-    } else {
-      element.classList.remove("selected");
-    }
-  }
-  removeDivs(divToShow);
-};
+import { useState } from "react";
 
 function Header() {
+  const [title, setTitle] = useState("Choose ");
+
+  const BODY_BACKGROUND_NASA_DIV = "bodybackgroundnasadiv";
+  const BODY_BACKGROUND_DOG_DIV = "bodybackgrounddogdiv";
+  const BODY_BACKGROUND_POKEMON_DIV = "bodybackgroundpokemondiv";
+  const BODY_BACKGROUND_GOOGLE_DIV = "bodybackgroundgooglediv";
+
+  const removeDivs = (div) => {
+    // let divArray = ["nasadiv", "dogdiv", "pokemondiv", "googlediv"];
+    let divArray = ["nasadiv", "dogdiv", "pokemondiv"];
+    let arrayWithoutExcludedDiv = [];
+    divArray
+      .filter((e) => e !== div)
+      .map((e) => arrayWithoutExcludedDiv.push(e));
+
+    arrayWithoutExcludedDiv.forEach((e) => {
+      let divToRemove = document.getElementsByClassName(e);
+      if (divToRemove[0].classList.contains(`${e}show`)) {
+        divToRemove[0].classList.remove(`${e}show`);
+      }
+    });
+    document.body.classList.remove(
+      BODY_BACKGROUND_NASA_DIV,
+      BODY_BACKGROUND_DOG_DIV,
+      BODY_BACKGROUND_POKEMON_DIV,
+      BODY_BACKGROUND_GOOGLE_DIV
+    );
+    const pokestring = `bodybackground${div}`;
+    document.body.classList.add(`bodybackground${div}`);
+    let divToShow = document.getElementsByClassName(div);
+    divToShow[0].classList.add(`${div}show`);
+  };
+
+  const hideShow = (apiClicked, divToShow) => {
+    let buttons = document.getElementsByClassName("navbutton");
+    for (const element of buttons) {
+      if (element.innerHTML === apiClicked) {
+        element.classList.add("selected");
+      } else {
+        element.classList.remove("selected");
+      }
+    }
+    setTitle(apiClicked);
+    removeDivs(divToShow);
+  };
+
   return (
     <header className="Header">
-      <div className="Header-title"> API Fetcher </div>
+      <div className="Header-title">{title} API</div>
       <nav className="navbar">
         <button
           className="navbutton"
